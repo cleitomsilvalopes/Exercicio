82bbebb144f1f2f445f29cd5f62e6a5bd5fa4fce
cleitomsilvalopes <cleitomsilvalopes4@gmail.com>
2026-06-12 12:23:09 -0300

    comitando

--- Execicio/Carro.java
+++ Execicio/Carro.java
@@ -1,9 +1,10 @@
+import javax.swing.JOptionPane;
 public class Carro extends Veiculos{
-    private int quantidadePassageiros;
+private int quantidadePassageiros;
 
-    public Carro(String placa, String modelo, double capacidadeTanque, double consumoKm, int quantidadePassageiros){
-        super(placa, modelo, capacidadeTanque, consumoKm);
-        this.quantidadePassageiros = quantidadePassageiros;
-        System.out.println("placa: " + placa + "\nModelo: " + modelo + "\nCapacidade de tanque: " + capacidadeTanque + "\nConsumo: " + consumoKm + "\nCapacidade de passageiros: " + quantidadePassageiros);
+public Carro(String placa, String modelo, double capacidadeTanque, double consumoKm, int quantidadePassageiros){
+    super(placa, modelo, capacidadeTanque, consumoKm);
+    this.quantidadePassageiros = quantidadePassageiros;
+        
     }
 }
