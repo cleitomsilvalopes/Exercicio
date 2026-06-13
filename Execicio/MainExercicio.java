dd08f3a053e2e4b5e1b7eb25bdc2cfc42a9fe2e1
cleitomsilvalopes <cleitomsilvalopes4@gmail.com>
2026-06-12 22:34:48 -0300

    adicionando metodo scanner

--- Execicio/MainExercicio.java
+++ Execicio/MainExercicio.java
@@ -1,9 +1,9 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 public class MainExercicio{
     public static void main(String[] args){
+        Scanner entrada = Scanner(System.in);
         
-        while(true){
-            
-        }
     }
 }
