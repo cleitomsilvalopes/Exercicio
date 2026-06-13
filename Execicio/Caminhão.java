ab74adac3b98aae6e9b3c75655301699c2069869
cleitomsilvalopes <cleitomsilvalopes4@gmail.com>
2026-06-13 19:49:44 -0300

    iunin

--- Execicio/Caminhão.java
+++
@@ -1,9 +0,0 @@
-public class Caminhão extends Veiculos{
-    private double cargaEmToneladas;
-
-    public Caminhão(String placa, String modelo, double capacidadeTanque, double consumoKm, double caragaEmToneladas){
-    super(placa, modelo, capacidadeTanque, consumoKm);
-    this.cargaEmToneladas = cargaEmToneladas;
-    System.out.println("placa: " + placa + "\nModelo: " + modelo + "\nCapacidade de tanque: " + capacidadeTanque + "\nConsumo: " + consumoKm + "\nCapacidade de passageiros: " + cargaEmToneladas);
-    }
-}
--- Execicio/Carro.java
+++
@@ -1,9 +0,0 @@
-public class Carro extends Veiculos{
-    private int quantidadePassageiros;
-
-    public Carro(String placa, String modelo, double capacidadeTanque, double consumoKm, int quantidadePassageiros){
-        super(placa, modelo, capacidadeTanque, consumoKm);
-        this.quantidadePassageiros = quantidadePassageiros;
-        System.out.println("placa: " + placa + "\nModelo: " + modelo + "\nCapacidade de tanque: " + capacidadeTanque + "\nConsumo: " + consumoKm + "\nCapacidade de passageiros: " + quantidadePassageiros);
-    }
-}
