28b7df6b828dfe0ac9d7260ec086c54446c0d39d
cleitomsilvalopes <cleitomsilvalopes4@gmail.com>
2026-06-13 12:11:16 -0300

    Update MainExercicio.java

--- Execicio/MainExercicio.java
+++ Execicio/MainExercicio.java
@@ -1,9 +1,18 @@
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 public class MainExercicio{
     public static void main(String[] args){
-        Scanner entrada = Scanner(System.in);
-        
+        while(true){
+            String placa = JOptionPane.showInputDialog(null, "Informe a placa", "Adicionando placa", JOptionPane.QUESTION_MESSAGE);
+            String modelo = JOptionPane.showInputDialog(null, "Informe o modelo: ", "Modelo", JOptionPane.QUESTION_MESSAGE);
+            double capacidadeDoTanque = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a capacidade de litros no tanque:", "Capacidade Maxima Do Tanque", JOptionPane.QUESTION_MESSAGE));
+            double consumo_por_km = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe quantos km faz por litro: ", "Consumo quilometrado", JOptionPane.QUESTION_MESSAGE));
+            String saida = JOptionPane.showInputDialog(null, "Deseja adicionar mais carros ? ", "Adicionar automoveis", JOptionPane.QUESTION_MESSAGE);
+            
+            if(saida.equalsIgnoreCase("não")){
+                break;
+            }
+        }
+        Carro carro = new Carro(placa, modelo, capacidadeDoTanque, consumo_por_km);
     }
-}
+} 
